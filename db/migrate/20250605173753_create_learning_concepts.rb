417dd8ce11d9a6db3241d2190bbaaeb3bfa64c58
briankim <brianhkim13@gmail.com>
2025-06-06 16:40:30 -0500

    fixed migrations

--- db/migrate/20250605173753_create_learning_concepts.rb
+++ db/migrate/20250605173753_create_learning_concepts.rb
@@ -1,9 +1,9 @@
 class CreateLearningConcepts < ActiveRecord::Migration[8.0]
   def change
-    create_table :learning_concepts do |t|
+    create_table :learning_concepts, id: false do |t|
+      t.integer :id, null: false, primary_key: true
       t.string :name
       t.text :description
-      t.integer :id, null: false, primary_key: true
 
       t.timestamps
     end
